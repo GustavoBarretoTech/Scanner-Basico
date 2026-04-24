24a0a618c16a3314a8504209c65083e40b1981f5
Gustavo Barreto <gu.lucabarreto@gmail.com>
2026-04-23 23:36:05 -0300

    Clarify comments for subprocess execution
    Updated comments to clarify the purpose of subprocess calls for both Linux and Windows.

--- Shell_Powershell.py
+++ Shell_Powershell.py
@@ -6,12 +6,12 @@ sistema = platform.system()
 
 if sistema == "Linux":
     print("Executando comando Shell no Linux...")
-    # O 'shell=True' permite que você escreva o comando como uma frase comum
+    #disparando o Shell script
     subprocess.run("nmcli device wifi show-password", shell=True) 
 
 elif sistema == "Windows":
     print("Executando comando PowerShell no Windows...")
-    # No Windows, você precisa chamar o powershell.exe e passar o comando
+    # disparando o Powershell
     comando_ps = "Get-Process | select -first 10"
     subprocess.run(["powershell", "-Command", comando_ps], shell=True)
 
